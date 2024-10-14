e1d3a5957cdb59e36e00b0e49acd3941bb89e244
Andrii <zerteler@gmail.com>
2024-10-14 23:50:51 +0300

    some fix

--- NER_manual_annotation/anot.py
+++ NER_manual_annotation/anot.py
@@ -1,32 +1,32 @@
 import json
 
 def convert_format(input_file, output_file):
-    # Читаємо початковий JSON файл
+    # Read the initial JSON file
     with open(input_file, 'r', encoding='utf-8') as f:
         data = json.load(f)
     
-    # Отримуємо список анотацій
+    # Get the list of annotations
     annotations = data.get("annotations", [])
     
-    # Новий формат даних
+    # New data format
     new_format = []
     
-    # Конвертуємо кожну анотацію
+    # Convert each annotation
     for annotation in annotations:
         if annotation and isinstance(annotation, list) and len(annotation) >= 2:
             text = annotation[0]
             entities = annotation[1].get("entities", [])
             
-            # Створюємо новий запис
+            # Create a new entry
             new_format.append([text, {"entities": entities}])
         else:
-            print(f"Пропущено некоректну анотацію: {annotation}")
+            print(f"Skipped invalid annotation: {annotation}")
     
-    # Записуємо дані у новий JSON файл
+    # Write the data to a new JSON file
     with open(output_file, 'w', encoding='utf-8') as f:
         json.dump(new_format, f, ensure_ascii=False, indent=2)
 
-# Використання функції
-input_file = r'D:\Tets_task1\data\annotations (4).json'  # Використання сирого рядка
-output_file = r'D:\Tets_task1\data\outpu_m2.json'
+# Usage of the function
+input_file = r'D:\(NER) model for the identification of mountain names\NER_manual_annotation\train_data\raw_annotations.json'  # Use of raw string
+output_file = r'D:\(NER) model for the identification of mountain names\NER_manual_annotation\train_data\annotations.json'
 convert_format(input_file, output_file)
